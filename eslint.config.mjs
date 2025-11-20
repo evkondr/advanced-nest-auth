e96ad7504b9c88a87ac3e225b63896c3ea1e6f1c
evkondr <kondratiev.evg@gmail.com>
2025-11-20 22:15:09 +0300

    feat: add start config in main

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,8 +27,9 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+  app.use(cookieParser(config.getOrThrow('COOKIE_SECRET')));
+  app.useGlobalPipes( new ValidationPipe({
+    transform: true 
+  }));
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['Set-Cookie']
+  });
+  await app.listen(config.getOrThrow<number>('APP_PORT'));
 }
 bootstrap();
