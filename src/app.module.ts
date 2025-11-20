985a7bcf499b2ae5964238c226a417eaa493c1b5
evkondr <kondratiev.evg@gmail.com>
2025-11-21 00:24:12 +0300

    feat: add ConfigModule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { IS_DEV_ENV } from './utils/is-dev.util';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    ignoreEnvFile: !IS_DEV_ENV,
+    isGlobal: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
