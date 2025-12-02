6511a86b9b5625b55d9936540aa3a81b6034d18e
evkondr <kondratiev.evg@gmail.com>
2025-12-02 19:18:21 +0300

    feat: add code for user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,7 @@
 import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { hash } from 'argon2';
+import { AuthMethod } from 'generated/prisma/enums';
 
 @Injectable()
 export class UserService {
@@ -14,6 +16,41 @@ export class UserService {
       }
     });
     if(!user) throw new NotFoundException('User not found');
+    return user;
+  }
+  public async findByEmail(email: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email
+      },
+      include: {
+        accounts: true
+      }
+    });
+    if(!user) throw new NotFoundException('User not found');
+    return user;
+  }
+  public async create(
+    email:string,
+    password:string,
+    displayName:string,
+    picture:string,
+    method:AuthMethod,
+    isVerified:boolean,
+  ) {
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        password: password ? await hash(password) : '',
+        displayName,
+        picture,
+        method,
+        isVerified
+      },
+      include: {
+        accounts: true  
+      }
+    });
     return user;
   }
 }
