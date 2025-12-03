ffc7fba253df8be04bc9270b7aa5645a01696bd2
evkondr <kondratiev.evg@gmail.com>
2025-12-03 23:52:59 +0300

    feat: add register in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import { UserService } from '@/user/user.service';
+import { AuthMethod } from 'generated/prisma/enums';
 
 @Injectable()
 export class AuthService {
-  public async register(dto): Promise<any> {}
+  constructor(private readonly userService:UserService){}
+  public async register(dto:RegisterDto): Promise<any> {
+    const isExist = await this.userService.findByEmail(dto.email);
+    if(isExist) {
+      throw new ConflictException('Пользователь с таким email уже существует');
+    }
+    const user = await this.userService.create(
+      dto.email,
+      dto.password,
+      dto.name, 
+      '', 
+      AuthMethod.CREDENTIALS, 
+      false
+    );
+    return user;
+  }
 
   public async login(user: any): Promise<any> {}
 
